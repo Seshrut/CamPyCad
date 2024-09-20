afccbb27c689397a1f042a0c8f07dd4288b39fcb
seshrut <seshrutjain@gmail.com>
2024-09-20 10:44:38 +0530

    vidtry

--- vidtry.py
+++ vidtry.py
@@ -17,18 +17,25 @@ def change_brightness(img, value=30):
     img = cv.cvtColor(final_hsv, cv.COLOR_HSV2BGR)
     return img
 i = 0
+l = []
 capture = cv.VideoCapture(0) # replace 0 with video location
 while True:
-    i += 1
     isTrue, frame = capture.read()
     # frame_resized = rescale(frame)
     gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
     canny = cv.Canny(gray,150,175)
+    l.append(canny)
     cv.imshow('orig',frame)
     cv.imshow('Video', canny)
     # cv.imshow('Video_resized', frame_resized)
-    if i%60==1:
-        pass
+    i += 1
+    addframe = 0
+    if i%15==1:
+        for frame in l:
+            addframe+=frame
+        avgframe = addframe/15
+        cv.imshow('eh',avgframe)
+        l = []
     if cv.waitKey(20) & 0xFF==ord('d'):
         break
 capture.release()
